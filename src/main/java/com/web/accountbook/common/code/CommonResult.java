fd749002498b30dec253b41a47688d74120ba0d6
shinsoonho <tnsghek12@naver.com>
2024-04-16 23:51:23 +0900

    로그인 관련 예외처리 추가

--- src/main/java/com/web/accountbook/common/code/CommonResult.java
+++ src/main/java/com/web/accountbook/common/code/CommonResult.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommonResult {
-    E0000("0000", "성공");
+    E0000("0000", "성공"),
+    E0001("0001", "이미 존재하는 아이디 입니다."),
+    E0002("0002", "아이디 또는 비밀번호를 확인해 주세요."),
+    E9999("9999", "알 수 없는 에러입니다");
 
     private String code;
     private String message;
